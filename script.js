f275be3da94ae76982d1e87f980f6c36aa2fda49
HanSolo4U <117237355+HanSolo4U@users.noreply.github.com>
2023-12-06 09:57:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+changeColor(squareId){
+var square = document.getElementById.squareId
+var randomColor = getRandomColor()
+}
+getRandomColor(){
+    var letters = '0123456789ABCDEF';
+    var color = '#'
+    for (var i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
